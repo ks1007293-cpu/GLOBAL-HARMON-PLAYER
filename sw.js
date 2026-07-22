40a777b382a31a273af8c930975c414bf49a3037
ks1007293-cpu <ks1007293@gmail.com>
2026-07-22 13:50:12 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,11 +1,32 @@
+const CACHE_NAME = 'harmony-pro-v1';
+const ASSETS = [
+  '/GLOBAL-HARMON-PLAYER/',
+  '/GLOBAL-HARMON-PLAYER/index.html',
+  '/GLOBAL-HARMON-PLAYER/manifest.json'
+];
+
 self.addEventListener('install', (e) => {
-  e.waitUntil(self.skipWaiting());
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+  self.skipWaiting();
 });
 
 self.addEventListener('activate', (e) => {
-  e.waitUntil(self.clients.claim());
+  e.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(keys.map((k) => {
+        if (k !== CACHE_NAME) return caches.delete(k);
+      }));
+    })
+  );
+  self.clients.claim();
 });
 
 self.addEventListener('fetch', (e) => {
-  e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
+  e.respondWith(
+    caches.match(e.request).then((cachedResponse) => {
+      return cachedResponse || fetch(e.request);
+    })
+  );
 });
